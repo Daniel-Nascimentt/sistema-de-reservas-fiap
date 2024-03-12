b2edf60ac5cc21a1d3fbc8c24f1e2dab4a31cd9e
Daniel-Nasciment <daniel.fecaf@outlook.com>
2024-03-12 10:19:24 -0300

    Feat: Setup inicial ms servicos-opcionais

---
+++ ms-servicos-opcionais/src/main/java/br/com/fiap/servicos/hospedagem/msservicosopcionais/MsServicosOpcionaisApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.servicos.hospedagem.msservicosopcionais;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsServicosOpcionaisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsServicosOpcionaisApplication.class, args);
+	}
+
+}
