291f9298cb42fe21af25b1580d7f30b6cd716f95
Daniel-Nasciment <daniel.fecaf@outlook.com>
2024-03-12 20:11:49 -0300

    Feat: Setup inicial ms quartos

---
+++ ms-quartos/src/main/java/br/com/fiap/quartos/msquarto/MsQuartoApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.quartos.msquarto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsQuartoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsQuartoApplication.class, args);
+	}
+
+}
---
+++ ms-quartos/src/test/java/br/com/fiap/quartos/msquarto/MsQuartoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.quartos.msquarto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsQuartoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
