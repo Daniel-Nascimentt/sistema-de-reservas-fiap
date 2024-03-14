aa8fd12a9660d64fd7fd2bdbbefe28c19a1467c8
Daniel-Nasciment <daniel.fecaf@outlook.com>
2024-03-13 21:37:04 -0300

    Feat: Atualizando docker-compose e criando setup inicial do ms de reservas

---
+++ ms-reservas/src/test/java/br/com/fiap/reservas/msreservas/MsReservasApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.reservas.msreservas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsReservasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
